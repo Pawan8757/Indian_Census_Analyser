dc0ac9aef7b76b23d43ae0c5d2ac298323d76db5
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-03-21 22:08:46 +0530

    adding 1st problem

---
+++ UC-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IndianStateCensusAnalyserProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Indian State Census Analyzer Program");
+        }
+    }
+}
