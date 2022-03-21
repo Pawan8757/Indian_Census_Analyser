68c1172c70b4133bd28323b1bf88bc8defab7af0
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-03-21 22:11:12 +0530

    adding 2nd problem

---
+++ UC-2/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace IndianStateCensusAnalyserProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Indian State Census Analyzer Program");
+        }
+    }
+}
